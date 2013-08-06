8b571636e2353b4b4ba047e655e16a37a1f2c0b8
Alexander Abramenko <Mr.Abramenko@gmail.com>
2013-08-06 14:26:50 +0400

    - Removed unnecessary files

--- Experimentation/Book/21_RubyGem.rb
+++
@@ -1,2 +0,0 @@
-# RubyGems is a standardized packaging and installation framework for Ruby libraries and appli-
-# cations. RubyGems makes it easy to locate, install, upgrade, and uninstall Ruby packages.
